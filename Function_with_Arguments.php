aeaf54958901d57a347ac7ae666db409ab25412c
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-03-17 12:28:46 +0530

    Function_with_Arguments
    Function_with_Arguments in php

---
+++ Function_with_Arguments.php
@@ -0,0 +1,19 @@
+<?php
+
+/*$num1 = 20;
+$num2 = 53;*/
+
+
+function add ($num1,$num2){
+	echo $num1 + $num2;
+}
+add (20,53);
+
+echo "<br>";
+
+function displayDate ($day, $date, $year){
+	echo $day."/".$date. "/".$year;
+}
+displayDate("Sunday",17,2019);
+
+?>
